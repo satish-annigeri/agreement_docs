bd6842e4d571c00f38261bb0f772601e5ace5866
Satish Annigeri <satish.annigeri@gmail.com>
2025-04-07 18:13:18 -0700

    Added with_suffix() function

--- utils.py
+++ utils.py
@@ -1,6 +1,11 @@
 from os.path import splitext
 
 
+def with_suffix(fpath: str, suffix: str) -> str:
+    base, _ = splitext(fpath)
+    return base + suffix
+
+
 def tpl_suffix(tpl_fname: str):
     basename, suffix = splitext(tpl_fname)
     if suffix == ".jinja":
