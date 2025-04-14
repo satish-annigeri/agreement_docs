da59929266db060e42bf021266f778fb4d4c9e17
Satish Annigeri <satish.annigeri@gmail.com>
2025-04-14 10:40:23 -0700

    Added Streamlit app

--- app.py
+++ app.py
@@ -1,199 +1,146 @@
-import marimo
-
-__generated_with = "0.12.8"
-app = marimo.App(width="medium")
-
-
-@app.cell
-def _():
-    import marimo as mo
-    return (mo,)
+import sys
+import platform
+from time import perf_counter
 
 
-@app.cell
-def _():
-    from platform import system
-    platform = system()
-    return platform, system
+import streamlit as st
 
+import polars as pl
 
-@app.cell
-def _():
-    from rich.console import Console
-    import polars as pl
-    from jinja2 import Environment, FileSystemLoader
-
-    con = Console()
-    env = Environment(loader=FileSystemLoader(""))
-    return Console, Environment, FileSystemLoader, con, env, pl
-
-
-@app.cell
-def _():
-    from mergedata import (read_data, prepare_data, group_data, unique_rows, extract_distributor_data, extract_exhibitor_data, extract_annexure_data)
-    return (
-        extract_annexure_data,
-        extract_distributor_data,
-        extract_exhibitor_data,
-        group_data,
-        prepare_data,
-        read_data,
-        unique_rows,
-    )
-
-
-@app.cell
-def _():
-    from htmlmerge import md_html_mergefields
-    return (md_html_mergefields,)
-
-
-@app.cell
-def _():
-    from utils import tpl_suffix
-    return (tpl_suffix,)
-
-
-@app.cell
-def _(mo):
-    d_file = mo.ui.file_browser(filetypes=['.xlsx'], multiple=False)
-    d_file
-    return (d_file,)
-
-
-@app.cell
-def _(mo):
-    e_file = mo.ui.file_browser(filetypes=['.xlsx'], multiple=False)
-    e_file
-    return (e_file,)
-
-
-@app.cell
-def _(mo):
-    t_file = mo.ui.file_browser(filetypes=['.xlsx'], multiple=False)
-    t_file
-    return (t_file,)
-
-
-@app.cell
-def _(d_file, e_file, t_file, tpl_suffix):
-    distributors_fname = d_file.path()
-    exhibitors_fname = e_file.path()
-    theatres_fname = t_file.path()
-    tpl_fname = "agreement.html.jinja"
-    tpl_type = tpl_suffix(tpl_fname)
-    print(f"Template type: {tpl_type}")
-    return (
-        distributors_fname,
-        exhibitors_fname,
-        theatres_fname,
-        tpl_fname,
-        tpl_type,
-    )
-
-
-@app.cell
-def _(distributors_fname, exhibitors_fname, read_data, theatres_fname):
-    distributors, exhibitors, theatres = read_data(distributors_fname, exhibitors_fname, theatres_fname, verbose=False)
-    return distributors, exhibitors, theatres
-
-
-@app.cell
-def _(
-    con,
-    distributors,
-    exhibitors,
-    extract_distributor_data,
-    group_data,
+from mergedata import (
     prepare_data,
-    theatres,
+    group_data,
     unique_rows,
-):
-    distributors_df, df = prepare_data(distributors, exhibitors, theatres)
-    distributor_data = extract_distributor_data(distributors_df)
-
-    group_cols = [
-        "exhibitor",
-        "exhibitor_place",
-        "movie",
-        "release_date",
-        "agreement_date",
-    ]
-    grouped_df = group_data(df, group_cols)
-    num_groups = unique_rows(df, group_cols)
-    print(f"Number of groups: {num_groups}")
-    con.print("Distributor data:")
-    con.print(distributor_data)
-    return (
-        df,
-        distributor_data,
-        distributors_df,
-        group_cols,
-        grouped_df,
-        num_groups,
-    )
-
-
-@app.cell
-def _(
-    distributor_data,
-    env,
-    extract_annexure_data,
+    extract_distributor_data,
     extract_exhibitor_data,
-    grouped_df,
-    md_html_mergefields,
-    tpl_fname,
-    tpl_type,
-):
-    count = 0
-    flist = []
-    fname_tpl = "{count:02}_{movie}_{exhibitor}_{release_date}"
+    extract_annexure_data,
+)
+from utils import tpl_suffix, get_fname
+from htmlmerge import md_html_mergefields, get_jinja2_template
+from docxmerge import docx_mergefields, docx2pdf_linux, docx2pdf_windows
 
 
-    if tpl_type in ['html', 'md']:
-        jinja_tpl = env.get_template(tpl_fname)
-        css_fname = "agreement.css"
-    
-    for g_exhibitors, g_theatres in grouped_df:
-        exhibitor_data = extract_exhibitor_data(g_exhibitors, g_theatres)
-        annexure = extract_annexure_data(g_theatres)
+t_start = perf_counter()
+st.title("Agreement Document Generator App")
+
+# Data files
+distributors_fname = "distributors.xlsx"
+exhibitors_fname = "exhibitors.xlsx"
+theatres_fname = "chhaava_theatres.xlsx"
+template_fname = "agreement.html.jinja"
+# template_fname = "agreement_template.docx"
+tpl_type = tpl_suffix(template_fname)
+css_fname = "agreement.css" if tpl_type in ["html", "md"] else None
+
+
+# Read data
+distributors = pl.read_excel(distributors_fname)
+exhibitors = pl.read_excel(exhibitors_fname)
+theatres = pl.read_excel(theatres_fname)
+
+msg = f"""### Input files
+
+* Distributors data: {distributors_fname}
+* Exhibitors data: {exhibitors_fname}
+* Theatres data: {theatres_fname}
+* Template file: {template_fname}
+* Template type: {tpl_type}"""
+if css_fname:
+    msg += f"""\n* CSS file: {css_fname}"""
+st.markdown(msg)
+
+if tpl_type in ["md", "html"]:
+    st.write(f"CSS file: {css_fname}")
+
+# Clean and prepare data for use
+distributors, df = prepare_data(distributors, exhibitors, theatres)
+
+group_cols = [
+    "exhibitor",
+    "exhibitor_place",
+    "movie",
+    "release_date",
+    "agreement_date",
+]
+grouped_df = group_data(df, group_cols)
+num_groups = unique_rows(df, group_cols)
+st.write(f"Number of unique groups: {num_groups}")
+
+fname_tpl = "{count:02}_{movie}_{exhibitor}_{release_date}"
+
+distributor_data = extract_distributor_data(distributors)
+
+count = 0
+flist = []
+if tpl_type in ["md", "html"]:
+    jinja_template = get_jinja2_template(template_fname, "")
+
+# --------------------------
+with st.status("Generating agreement document files...", expanded=True) as status:
+    for g_exhibitor, g_theatres in grouped_df:
         count += 1
-        output_fname = fname_tpl.format(
+        exhibitor_data = extract_exhibitor_data(g_exhibitor, g_theatres)
+        annexure = extract_annexure_data(g_theatres)
+
+        output_fname = get_fname(
+            fname_tpl,
             count=count,
             movie=exhibitor_data["movie"].lower(),
-            exhibitor=exhibitor_data["exhibitor"].replace("/", "").replace(" ", "_").lower(),
-            release_date=exhibitor_data['release_date']
+            exhibitor=exhibitor_data["exhibitor"],
+            release_date=exhibitor_data["release_date"],
         )
-        match tpl_type:
-            case 'docx':
-                print(f"Microsoft Word: {output_fname}.docx")
-            case 'html' | 'md':
-                print(f"HTML/Markdown: {output_fname}.pdf")
-                md_html_mergefields(
-                    jinja_tpl,
-                    tpl_type,
-                    css_fname,
-                    f"{output_fname}.pdf",
-                    distributor_data=distributor_data,
-                    exhibitor_data=exhibitor_data,
-                    annexure=annexure,
-                )
-            case _:
-                print('Unknown template type')
+
+        if tpl_type == "docx":
+            output_fname = f"{output_fname}.docx"
+            docx_mergefields(
+                template_fname,
+                output_fname,
+                distributor_data,
+                exhibitor_data,
+                annexure,
+            )
+            st.write(f"Generating {output_fname}")
+        elif tpl_type in ["html", "md"]:
+            output_fname = f"{output_fname}.pdf"
+            md_html_mergefields(
+                jinja_template,
+                tpl_type,
+                css_fname,
+                output_fname,
+                distributor_data=distributor_data,
+                exhibitor_data=exhibitor_data,
+                annexure=annexure,
+            )
+            st.write(f"Generating {output_fname}")
+        else:
+            sys.exit(1)
         flist.append(output_fname)
-    return (
-        annexure,
-        count,
-        css_fname,
-        exhibitor_data,
-        flist,
-        fname_tpl,
-        g_exhibitors,
-        g_theatres,
-        jinja_tpl,
-        output_fname,
+    t2 = perf_counter()
+    status.update(
+        label=f"Generation of agreement files is complete in {t2 - t_start:.2f}s at {(t2 - t_start) / num_groups:.2f}s per file",
+        state="complete",
+        expanded=False,
     )
-
-
-if __name__ == "__main__":
-    app.run()
+    # --------------------------
+if tpl_type in ["md", "html"]:
+    t_stop = perf_counter()
+elif tpl_type == "docx":
+    with st.status(
+        "Converting Microsoft Word documents to PDF...", expanded=True
+    ) as status:
+        match platform.system():
+            case "Linux":
+                docx2pdf_linux(flist)
+            case "Windows":
+                docx2pdf_windows(flist)
+            case _:
+                raise OSError
+        t_stop = perf_counter()
+    status.update(
+        label=f"Generation of PDF files is complete in {t_stop - t2:.2f}s at {(t_stop - t_start) / num_groups:.2f}s per file",
+        state="complete",
+        expanded=False,
+    )
+    st.write(
+        f"Total time taken: {t_stop - t_start:.2f}s at {(t_stop - t_start) / num_groups:.2f}s per file"
+    )
